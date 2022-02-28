6612d9fa76abc454fe78bfdbe7c5b62e19fd9206
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-28 14:04:53 +0000

    addNewComment functionality added

--- src/components/CreateComment.jsx
+++ src/components/CreateComment.jsx
@@ -1,8 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CommentContext from "../context/CommentContext";
 
 const CreateComment = () => {
-  const { currentUser } = useContext(CommentContext);
+  const { currentUser, addNewComment } = useContext(CommentContext);
+  const [text, setText] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addNewComment(text);
+    setText("");
+  };
   return (
     <div className="comment createComment">
       <img
@@ -10,12 +17,14 @@ const CreateComment = () => {
         alt="User"
         className="avatar comment"
       />
-      <form className="commentForm">
+      <form className="commentForm" onSubmit={handleSubmit}>
         <textarea
           className="inputComment"
           name="comment"
           placeholder="Add a comment..."
-          style={{ height: "65px", width: "510px" }}
+          style={{ height: "4rem", width: "80%" }}
+          onChange={(event) => setText(event.target.value)}
+          value={text}
         />
         <button className="submit" type="submit">
           SEND
--- src/context/CommentContext.js
+++ src/context/CommentContext.js
@@ -4,14 +4,32 @@ import data from "../data/data.json";
 const CommentContext = createContext();
 
 export const CommentContextProvider = ({ children }) => {
-  const [comments, setComments] = useState(data);
+  const [state, setState] = useState(data);
+
+  const { comments, currentUser } = state;
+
+  const addNewComment = (comment) => {
+    console.log(comments.at(comments.length - 1).id);
+    const newComment = {
+      id: comments.at(comments.length - 1).id + 1,
+      content: comment,
+      // Change time to calculated field
+      createdAt: "5 seconds ago",
+      replies: [],
+      score: 6,
+      user: currentUser,
+    };
+    comments.push(newComment);
+    setState((prevState) => ({ ...prevState, comments }));
+    console.log(state);
+  };
 
   useEffect(() => {
-    console.log(comments);
-  }, [comments]);
+    console.log(state);
+  }, [state]);
 
   return (
-    <CommentContext.Provider value={{ ...comments }}>
+    <CommentContext.Provider value={{ ...state, addNewComment }}>
       {children}
     </CommentContext.Provider>
   );
