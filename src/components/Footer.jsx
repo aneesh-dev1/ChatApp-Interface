4221f8d5c4e28081aa3332d21204fcb294a968a8
Aneesh Kumar <71940535+aneesh-devop@users.noreply.github.com>
2022-03-06 22:59:51 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,21 +2,13 @@ const Footer = () => {
   return (
     <footer>
       <p>
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        &nbsp; &nbsp;|| &nbsp; Coded by{" "}
+        Coded by{" "}
         <a
           href="https://github.com/Quarcoo-arc/interactive-coments-section"
           target="_blank"
           rel="noreferrer"
         >
-          Minoq 😇
+          Aneesh Kumar
         </a>
         .
       </p>
